2f4e27f1f770539d22a88fe8f59d5a86b49fad72
PaniniTheBest <bujito2003@gmail.com>
2025-11-18 20:47:32 +0800

    Create_3D_Cube (code replaced with Nicholas' code)
    accidently used Chubie's code in the previous update

--- Fly_By_B(eat)/_3D_Shapes.cpp
+++ Fly_By_B(eat)/_3D_Shapes.cpp
@@ -1,94 +1,64 @@
 #include "_3D_Shapes.h"
 
-void _3D_Shapes::Create_3D_Cube(float x, float y, float z)
+void Create_3D_Cube(float x, float y, float z)
 {
-	glBegin(GL_QUADS); //RED SIDE Z-based//
-	glVertex3f(-1, 1, -1);
-	glVertex3f(1, 1, -1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(-1, -1, -1);
-	/*if (z > 0)
-	{
-		glColor3f(1.0, 0, 0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glBegin(GL_QUADS);// Face 1 Z-axis
+    if (z < 0)
+        glColor3f(0.3f, 1.0f, 0.0f); //green
 
-	glBegin(GL_QUADS); //CYAN SIDE X-based//
-	glVertex3f(-1, 1, 1);
-	glVertex3f(1, 1, 1);
-	glVertex3f(1, -1, 1);
-	glVertex3f(-1, -1, 1);
-	/*if (x < 0)
-	{
-		glColor3f(0, 1.0, 1.0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
 
-	glBegin(GL_QUADS); //YELLOW SIDE X-based//
-	glVertex3f(-1, 1, 1);
-	glVertex3f(-1, -1, 1);
-	glVertex3f(-1, -1, -1);
-	glVertex3f(-1, 1, -1);
-	/*if (x > 0)
-	{
-		glColor3f(1.0, 1.0, 0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glBegin(GL_QUADS);// Face 2 Z-axis
+    if (z > 0)
+        glColor3f(0.5f, 0.5f, 1.0f); //blue
 
-	glBegin(GL_QUADS); //GREEN SIDE Y-based//
-	glVertex3f(1, 1, 1);
-	glVertex3f(1, -1, 1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(1, 1, -1);
-	/*if (y < 0)
-	{
-		glColor3f(0, 1.0, 0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glEnd();
 
-	glBegin(GL_QUADS); // BLUE SIDE Y-based//
-	glVertex3f(-1, -1, 1);
-	glVertex3f(1, -1, 1);
-	glVertex3f(1, -1, -1);
-	glVertex3f(-1, -1, -1);
-	/*if (y > 0)
-	{
-		glColor3f(0, 0, 1.0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glBegin(GL_QUADS);// Face 3 X-axis
+    if (x > 0)
+        glColor3f(1.0f, 0.3f, 0.0f); //orange
 
-	glBegin(GL_QUADS); //PURPLE SIDE Y-based//
-	glVertex3f(-1, 1, 1);
-	glVertex3f(1, 1, 1);
-	glVertex3f(1, 1, -1);
-	glVertex3f(-1, 1, -1);
-	/*if (z > 0)
-	{
-		glColor3f(1.0, 0, 1.0);
-	}
-	else
-	{
-		glColor3f(1.0, 1.0, 1.0);
-	}*/
-	glEnd();
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glEnd();
+
+    glBegin(GL_QUADS);// Face 4 X-axis
+    if (x < 0)
+        glColor3f(1.0f, 0.0f, 0.0f); //red
+
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glEnd();
+
+    glBegin(GL_QUADS);// Face 5 Y-axis 
+    if (y < 0)
+        glColor3f(1.0f, 0.9f, 0.0f); //yellow
+
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
+
+    glBegin(GL_QUADS);// Face 6 Y-axis 
+    if (y > 0)
+        glColor3f(1.0f, 0.9f, 0.8f); //pearl-white
+
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glEnd();
 }
