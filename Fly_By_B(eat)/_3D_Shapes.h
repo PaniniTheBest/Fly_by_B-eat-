c49e787393c110c09d3d0ac6916a2dd8a2255dea
PaniniTheBest <bujito2003@gmail.com>
2025-11-20 11:16:36 +0800

    Private deleted

--- Fly_By_B(eat)/_3D_Shapes.h
+++ Fly_By_B(eat)/_3D_Shapes.h
@@ -9,16 +9,7 @@
 using namespace std;
 class _3D_Shapes
 {	
-	private:
-		float X, Y, Z;
 	public:
-		/*_3D_Shapes();
-		_3D_Shapes(float x, float y, float z)
-		{
-			this -> X = x;
-			this -> Y = y;
-			this -> Z = z;
-		}*/
 		void Move_Object (float x, float y, float z);
 		void Create_3D_Cube(float x, float y, float z);
 		void Create_3D_Sphere(float x, float y, float z);
