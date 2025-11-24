cecfb8f8f73dd32abb47e92e088c8147154fad05
jokr-430 <notjoker351@gmail.com>
2025-11-24 16:16:03 +0800

    Update pt 2

--- Fly_By_B(eat)/Main.cpp
+++ Fly_By_B(eat)/Main.cpp
@@ -10,54 +10,6 @@ float rotateAngle = 0.0f;
 //GameObject player;
 //camera cam;
 
-//void RenderScene(void)
-//{
-//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//    glLoadIdentity();
-//
-//    gluLookAt(  0.0f, 0.0f, 50.0f,
-//                0.0f, 0.0f, 0.0f,
-//                0.0f, 1.0f, 0.0f);
-//
-//    //cam.ApplyCamera();
-//    glTranslatef(0.0f, 3.0f, 0.0f);
-//    //glRotatef(rotateAngle, 3.0f, 4.0f, 2.0f);
-//    //==============================================
-//    //Render 3D objects here / / /
-//    _3D_Shapes ObjTest1, ObjTest_2;
-//
-//    ObjTest1.Transform_Object_Position(-5.0f, 0.0f, 0.0f);
-//    ObjTest1.Transform_Object_Size(-1.0f,5.0f,0.0f);
-//    ObjTest1.Apply_Color(255, 0, 0);
-//    ObjTest1.Create_3D_Cube(2, 2, 5);
-//
-//    ObjTest_2.Transform_Object_Position(0.01f, 0.01f, 0.0f);
-//    ObjTest_2.Apply_Color(0, 255, 0);
-//    ObjTest_2.Create_3D_Cylinder(4.0f, 4.0f, 8);
-//    
-//    //==============================================
-//    rotateAngle += 1.0f;
-//    glutSwapBuffers();
-//
-//}
-//void ChangeSize(int w, int h)
-//{
-//    if (h == 0)
-//        h = 1;
-//
-//    float ratio = 1.0 * w / h;
-//
-//    //fix the matrixmode to projection 
-//    glMatrixMode(GL_PROJECTION);
-//    //Reset the value of matrices
-//    glLoadIdentity();
-//    //Set the viewport of the window
-//    glViewport(0, 0, w, h);
-//    gluPerspective(45, ratio, 1, 1000);
-//    // Get Back to Model View
-//    glMatrixMode(GL_MODELVIEW);
-//}
-
 //void processNormalKeys()
 //{
 //    const char keys[] = { 'w', 'a', 's', 'd' };
