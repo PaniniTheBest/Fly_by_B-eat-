4c501537b11de574e8335971105605f7e6e976dd
jokr-430 <notjoker351@gmail.com>
2025-11-24 15:26:33 +0800

    Created Engine files

---
+++ Fly_By_B(eat)/engine.h
@@ -0,0 +1,13 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "GameObject.h"
+#include "Input.h"
+#include <vector>
+
+//void Initialize();
+//void Update();
+//void PrepEngine(int argc, char** argv);
+
+
+#endif
