5333d30a83e0dd17b9ee1eac143cabee929bfdc1
jokr-430 <notjoker351@gmail.com>
2025-11-27 21:25:21 +0800

    Added ModelLoader files

---
+++ Fly_By_B(eat)/ModelData.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef MODEL_DATA_H
+#define MODEL_DATA_H
+
+#include "vector"
+using namespace std;
+
+// the pairing of vertex and normals
+struct Face {
+	vector<int> VertexIndex;
+	vector<int> NormalIndex;
+};
+#endif
