4723663dcbbfc0e4e38bfd913cbec269956f2d86
jokr-430 <notjoker351@gmail.com>
2025-11-28 11:35:42 +0800

    Added more functions in GameObject and disabled Graphics.cpp temporarily

--- Fly_By_B(eat)/GameObject.cpp
+++ Fly_By_B(eat)/GameObject.cpp
@@ -6,6 +6,7 @@ GameObject::GameObject()
 	position.SetValue(0, 0, 0);
 	scale.SetValue(1, 1, 1);
 	SetRotation(0, 0, 0, 0);
+	collider.SetDimension(position, scale);
 }
 
 void GameObject::SetPosition(Vector3 newPosition)
@@ -40,6 +41,16 @@ void GameObject::SetRotation(float angle, float x, float y, float z)
 	rotation.SetValue(x, y, z);
 }
 
+void GameObject::SetCollider(Vector3 position, Vector3 scale)
+{
+	collider.SetDimension(position, scale);
+}
+
+bool GameObject::CheckCollision(GameObject other)
+{
+	return collider.CheckCollision(other.collider);
+}
+
 Vector3 GameObject::GetScale()
 {
 	return scale;
@@ -49,3 +60,48 @@ Vector3 GameObject::GetPosition()
 {
 	return position;
 }
+
+Collider GameObject::GetCollider()
+{
+	return collider;
+}
+
+void GameObject::Draw()
+{
+	glPushMatrix();
+	glTranslatef(position.x, position.y, position.z);
+	glRotatef(angle, rotation.x, rotation.y, rotation.z);
+	glScalef(scale.x, scale.y, scale.z);
+	glutSolidCube(1.0f);
+	glPopMatrix();
+
+	collider.SetDimension(position, scale);
+}
+
+void GameObject::DrawSphere(float radius, int slices, int stacks)
+{
+	glPushMatrix();
+	glTranslatef(position.x, position.y, position.z);
+	glRotatef(angle, rotation.x, rotation.y, rotation.z);
+	glScalef(scale.x, scale.y, scale.z);
+	glutSolidSphere(radius, slices, stacks);
+	glPopMatrix();
+
+	collider.SetDimension(position, scale);
+}
+
+
+//void GameObject::LoadModel(string modelPath)
+//{
+//	model.LoadOBJ(modelPath);
+//}
+//
+//void GameObject::RenderModel()
+//{
+//	glPushMatrix();
+//	glTranslatef(position.x, position.y, position.z);
+//	glRotatef(angle, rotation.x, rotation.y, rotation.z);
+//	glScalef(scale.x, scale.y, scale.z);
+//	model.RenderModel();
+//	glPopMatrix();
+//}
--- Fly_By_B(eat)/GameObject.h
+++ Fly_By_B(eat)/GameObject.h
@@ -2,7 +2,8 @@
 #define GAMEOBJECT_H
 
 #include "vector3.h"
-#include "Render_3D_Shapes.h"
+#include "ModelLoader.h"
+#include "Collider.h"
 #include <glut.h>
 #include <cmath>
 #include <iostream>
@@ -20,16 +21,26 @@ public:
 	void SetScale(float x, float y, float z);
 	void SetRotation(float angle, Vector3 rotation);
 	void SetRotation(float angle, float x, float y, float z);
+	void SetCollider(Vector3 position, Vector3 scale);
+	bool CheckCollision(GameObject other);
 
 	Vector3 GetScale();
 	Vector3 GetPosition();
+	Collider GetCollider();
 
-	//void Draw();
+	void Draw();
+	void DrawSphere(float radius, int slices, int stacks);
+
+	//void LoadModel(string modelPath);
+	//void RenderModel();
+
 private:
 	Vector3 position;
 	Vector3 scale;
 	Vector3 rotation;
 	float angle;
+	Collider collider;
+	/*ModelLoader model;*/
 };
 
 #endif
