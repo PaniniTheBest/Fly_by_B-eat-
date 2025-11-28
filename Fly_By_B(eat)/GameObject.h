6ea4b2b484d03b7fd931dd30d61f0afca9c9aaa5
jokr-430 <notjoker351@gmail.com>
2025-11-28 14:39:31 +0800

    Added more functions to GameObject

--- Fly_By_B(eat)/GameObject.h
+++ Fly_By_B(eat)/GameObject.h
@@ -3,6 +3,7 @@
 
 #include "vector3.h"
 #include "Render_3D_Shapes.h"
+#include "Collider.h"
 #include <glut.h>
 #include <cmath>
 #include <iostream>
@@ -20,16 +21,23 @@ public:
 	void SetScale(float x, float y, float z);
 	void SetRotation(float angle, Vector3 rotation);
 	void SetRotation(float angle, float x, float y, float z);
+	void SetCollider(Vector3 position, Vector3 scale);
+	bool CheckCollision(GameObject other);
+
 
 	Vector3 GetScale();
 	Vector3 GetPosition();
+	Collider GetCollider();
 
-	//void Draw();
+	void Draw();
+	void DrawSphere(float radius, int slices, int stacks);
+
 private:
 	Vector3 position;
 	Vector3 scale;
 	Vector3 rotation;
 	float angle;
+	Collider collider;
 };
 
 #endif
